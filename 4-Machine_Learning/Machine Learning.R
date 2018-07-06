e31dae033b30f583c4c6d1880b4ba6c7f520ea61
Fadi Murad <muradfadi83@gmail.com>
2018-07-05 22:45:40 -0700

    2nd Modification on Machine Learning: Adding Naive Bayes model

--- 4-Machine_Learning/Machine Learning.R
+++ 4-Machine_Learning/Machine Learning.R
@@ -1,4 +1,3 @@
-library(caret)
 library(rJava)
 library(mlr)
 library(lattice)
@@ -22,12 +21,17 @@ tr_Under <- undersample(tr_Task,rate = 0.1) #keep only 10% of majority class
 #oversampling
 tr_Over <- oversample(tr_Task,rate=15) #multiply minority class 15 times
 
+tr_smote <- smote(tr_Task,rate = 10,nn = 3)
+
+
+
 #Validation Strategy
-res_Mod <- makeResampleDesc("CV",iters=4,stratify = TRUE)
+res_Mod <- makeResampleDesc("CV",iters=5,stratify = TRUE)
 
 
 #Logistic Reg Model:
-logR_learner <- makeLearner("classif.logreg",predict.type = "prob")
+logR_learner <- makeLearner("classif.logreg",predict.type = "prob", fix.factors.prediction = TRUE)
+
 
 logRCV_Task<- resample(logR_learner,tr_Task,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
 logRCV_Under<- resample(logR_learner,tr_Under,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
@@ -36,6 +40,9 @@ logRCV_Over<- resample(logR_learner,tr_Over,res_Mod,measures = list(auc,acc,tpr,
 AgglogRCV_Task<- logRCV_Task$aggr
 AgglogRCV_Under<- logRCV_Under$aggr
 AgglogRCV_Over<- logRCV_Over$aggr
+
+
+cbind(as.data.frame(AgglogRCV_Task),as.data.frame(AgglogRCV_Under),as.data.frame(AgglogRCV_Over))
 
 #remove resample objects to save memory
 remove(logRCV_Task,logRCV_Over,logRCV_Under)
@@ -59,15 +66,14 @@ logR_Pre <- predict(logR_Mod,test_Task)
 randF_learner <- makeLearner("classif.h2o.randomForest",predict.type = "prob")
 
 
+
 #cross function
 randFCV_Task<- resample(randF_learner,tr_Task,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
 randFCV_Under<- resample(randF_learner,tr_Under,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
 randFCV_Over<- resample(randF_learner,tr_Over,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
 
 #Summary of CV
-AggrandFCV_Task<-randFCV_Task$aggr
-AggrandFCV_Under<-randFCV_Under$aggr
-AggrandFCV_Over<-randFCV_Over$aggr
+cbind(as.data.frame(AggrandFCV_Task),as.data.frame(AggrandFCV_Under),as.data.frame(AggrandFCV_Over))
 
 remove(randFCV_Over,randFCV_Task,randFCV_Under)
 
@@ -77,95 +83,88 @@ remove(randFCV_Over,randFCV_Task,randFCV_Under)
 
 
 #Train & Prediction
-randF_Mod <- train(randF_learner, tr_Over)
-randF_Pre <- predict(randF_Mod,test_Task)
+randF_Mod_Over <- train(randF_learner, tr_Over)
+randF_Pre_Over <- predict(randF_Mod,test_Task)
 
+randF_Mod_Under <- train(randF_learner, tr_Under)
+randF_Pre_Under <- predict(randF_Mod1,test_Task)
 
-randF_Mod1 <- train(randF_learner, tr_Under)
-randF_Pre1 <- predict(randF_Mod1,test_Task)
+#performance measure for train models
+randF_Over_Mes<- mlr::performance(randF_Pre_Over,list(auc,acc,tpr,tnr,fpr))
+randF_Under_Mes<-mlr::performance(randF_Pre_Under,list(auc,acc,tpr,tnr,fpr))
+cbind(as.data.frame(randF_Over_Mes),as.data.frame(randF_Under_Mes))
 
-mlr::performance(randF_Pre,list(auc,acc,tpr,tnr,fpr,fp,fn))
-mlr::performance(randF_Pre1,list(auc,acc,tpr,tnr,fpr,fp,fn))
+# Over Model Confusion Matrix
+mlr::calculateConfusionMatrix(randF_Pre_Over)
 
+# Under Model Confusion Matrix
+mlr::calculateConfusionMatrix(randF_Pre_Under)
 
 #ROC
-randFROCR_Pre <- asROCRPrediction(randF_Pre)
-randFROCR_PreP <- ROCR::performance(randFROCR_Pre,"tpr","fpr")
-randFROCR_Pre1 <- asROCRPrediction(randF_Pre1)
-randFROCR_PreP1 <- ROCR::performance(randFROCR_Pre1,"tpr","fpr")
+randFROCR_Th <- generateThreshVsPerfData(list(Over = randF_Pre_Over, Under = randF_Pre_Under),
+                                         measures = list(tpr,fpr,ppv) )
 
-
-ROCR::plot(randFROCR_PreP,col = "red")
-ROCR::plot(randFROCR_PreP1,col = "blue",add = TRUE)
-legend("bottomright",legend = c("Over","Under"),lty=1, col=c("red","blue"))
+#ROC Curve:
+randFROCR_Th <- generateThreshVsPerfData(list(Over = randF_Pre_Over, Under = randF_Pre_Under),
+                                         measures = list(tpr,fpr,ppv) )
+gridExtra::grid.arrange(plotROCCurves(randFROCR_Th,measures = list(fpr,tpr),diagonal = FALSE)+ggtitle("AUC") ,
+                        plotROCCurves(randFROCR_Th,measures = list(tpr,ppv),diagonal = FALSE) + ggtitle("Precision vs Recall"),ncol = 2)
 
 
 #naive model
-naiveBa_learner <- makeLearner("classif.naiveBayes",predict.type = "prob")
+naiveBa_learner <- makeLearner("classif.naiveBayes",predict.type = "prob", fix.factors.prediction = TRUE)
 
-naiveBaCV_Task<- resample(naiveBa_learner,tr_Task,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
-naiveBaCV_Under<- resample(naiveBa_learner,tr_Under,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
-naiveBaCV_Over<- resample(naiveBa_learner,tr_Over,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
+help(classif.naiveBayes)
 
-naiveBaCV_Task$aggr
-naiveBaCV_Under$aggr
-naiveBaCV_Over$aggr
+naiveBaCV_Task<- resample(naiveBa_learner,tr_Task,res_Mod,measures = list(auc,acc,tpr,tnr,fpr))
+naiveBaCV_Under<- resample(naiveBa_learner,tr_Under,res_Mod,measures = list(auc,acc,tpr,tnr,fpr))
+naiveBaCV_Over<- resample(naiveBa_learner,tr_Over,res_Mod,measures = list(auc,acc,tpr,tnr,fpr))
 
+AggnaiveBaCV_Task<- naiveBaCV_Task$aggr
+AggnaiveBaCV_Under<- naiveBaCV_Under$aggr
+AggnaiveBaCV_Over<- naiveBaCV_Over$aggr
+
+remove(naiveBaCV_Task,naiveBaCV_Over,naiveBaCV_Under)
 
 #Train & Prediction
-naiveBa_Mod <- train(naiveBa_learner, tr_Over)
-naiveBa_Pre <- predict(naiveBa_Mod,test_Task)
-
-naiveBa_Mod1 <- train(naiveBa_learner, tr_Under)
-naiveBa_Pre1 <- predict(naiveBa_Mod1,test_Task)
+naiveBa_Mod_Over <- train(naiveBa_learner, tr_Over)
+naiveBa_Pre_Over <- predict(naiveBa_Mod_Over,test_Task)
 
 
-mlr::performance(naiveBa_Pre,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
-mlr::performance(naiveBa_Pre1,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
+naiveBa_Mod_Under <- train(naiveBa_learner, tr_Under)
+naiveBa_Pre_Under <- predict(naiveBa_Mod_Under,test_Task)
 
 
-naiveROCR_Pre <- asROCRPrediction(naiveBa_Pre)
-naiveROCR_PreP <- ROCR::performance(naiveROCR_Pre,"tpr","fpr")
+#performance measure for train models
+naiveBa_Over_Mes<- mlr::performance(naiveBa_Pre_Over,list(auc,acc,tpr,tnr,fpr))
+naiveBa_Under_Mes<-mlr::performance(naiveBa_Pre_Under,list(auc,acc,tpr,tnr,fpr))
+cbind(as.data.frame(naiveBa_Over_Mes),as.data.frame(naiveBa_Under_Mes))
 
-naiveROCR_Pre1 <- asROCRPrediction(naiveBa_Pre1)
-naiveROCR_PreP1 <- ROCR::performance(naiveROCR_Pre1,"tpr","fpr")
+# Over Model Confusion Matrix
+mlr::calculateConfusionMatrix(naiveBa_Pre_Over)
 
-ROCR::plot(naiveROCR_PreP,col = "red")
-ROCR::plot(naiveROCR_PreP1,col = "blue",add = TRUE)
-legend("bottomright",legend = c("Over","Under"),lty=1, col=c("red","blue"))
+# Under Model Confusion Matrix
+mlr::calculateConfusionMatrix(naiveBa_Pre_Under)
+
+
+
 
 
 #SVM model
 SVM_learner <- makeLearner("classif.svm",predict.type = "prob")
 
-SVMCV_Task<- resample(SVM_learner,tr_Task,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
-SVMCV_Under<- resample(SVM_learner,tr_Under,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
-SVMCV_Over<- resample(SVM_learner,tr_Over,res_Mod,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
+
+SVM_learner$par.vals <- list(class.weights = c("0"=1,"1"=10),kernel="radial")
+
+svm_param <- makeParamSet(makeIntegerParam("cost",lower = 0.1,upper = 100),
+                          makeIntegerParam("gamma",lower= 0.5,upper = 2))
 
 
+set_search <- makeTuneControlRandom(maxit = 5L)
 
-naiveBaCV_Task$aggr
-naiveBaCV_Under$aggr
-naiveBaCV_Over$aggr
-
-
-#Train & Prediction
-naiveBa_Mod <- train(naiveBa_learner, tr_Over)
-naiveBa_Pre <- predict(naiveBa_Mod,test_Task)
-
-performance(naiveBa_Pre,measures = list(auc,acc,tpr,tnr,fpr,fp,fn))
+svm_tune <- tuneParams(learner = SVM_learner,task = tr_Task,
+                       measures = list(acc,tpr,tnr,fpr,fp,fn), 
+                       par.set = svm_param,control = set_search,resampling = res_Mod)
 
 
-
-
-#ROC Plot
-randFROCR_Pre <- asROCRPrediction(randF_Pre)
-randFROCR_Pre1 <- ROCR::performance(randFROCR_Pre,"tpr","fpr")
-
-naiveROCR_Pre <- asROCRPrediction(naiveBa_Pre)
-naiveROCR_Pre1 <- ROCR::performance(naiveROCR_Pre,"tpr","fpr")
-
-ROCR::plot(randFROCR_Pre1,col = "red")
-ROCR::plot(naiveROCR_Pre1,col = "blue",add = TRUE)
-legend("bottomright",legend = c("randomForest","naiveBayes"),lty=1, col=c("red","blue"))
 
